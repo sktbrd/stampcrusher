ef0c524d4488a1f4d728d56ba73c3de36a9929ed
¨sktbrd¨ <sktbrd.eth@gmail.com>
2023-10-26 22:35:55 -0300

    I always knew

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-<div>
-<h1> 
-put out bids on commissioned art by artists that cant be easily contacted
-</h1>
-  <div class="button-container">
-<button style="background-color: blue;" class="custom-button">
-      Connect Your Butthole
-    </button>
+  <div>
+    <h1 style="color: black;">Put out bids on commissioned art by artists that can't be easily contacted</h1>
+    <div class="button-container">
+      <button @click="showAlert" style="background-color: blue;" class="custom-button">
+        Connect Your Butthole
+      </button>
+    </div>
   </div>
-</div>
   <div>
     <button class="custom-button">
       <img src="https://i.ibb.co/C56Q9Wj/image.png" alt="Image Placeholder" class="button-image">
     </button>
   </div>
 </template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// Define the function to show the alert
+const showAlert = () => {
+  alert('I always knew...');
+}
+</script>
 
 <style scoped>
 .custom-button {
@@ -28,36 +31,21 @@ put out bids on commissioned art by artists that cant be easily contacted
   margin: 0;
   cursor: pointer;
   border-radius: 10px;
-  padding: 10px
+  padding: 10px;
 }
 
 .button-image {
-  width:100%; 
-  height: auto; /* Set the height of the image */
-  object-fit: cover; /* Ensure the image covers the entire button area */
+  width: 100%;
+  height: auto;
+  object-fit: cover;
   border: none;
   padding: 0;
   margin: 0;
 }
-</style>
 
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 .button-container {
   position: absolute;
-  top: 20px; /* Adjust the top position as needed */
-  right: 20px; /* Adjust the right position as needed */
+  top: 20px;
+  right: 20px;
 }
 </style>
